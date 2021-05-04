1d12624da963def23a0f54216dfd84a9a7b59a97
Clay Jones <cgjones0711@gmail.com>
2021-05-03 21:49:49 -0500

    sql accepting data, but doesnt print on page

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,11 @@
 module.exports = {
-    format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
-    },
-    
-  };
-  
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
+  format_count: (count) => {
+    // format large numbers with commas
+    return parseInt(count);
+  },
+
+}
